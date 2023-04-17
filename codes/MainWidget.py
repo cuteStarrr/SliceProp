ea1b2e87d3347dea0ec3a1e2da0afb4b1c8e7c17
blueeeeeeeee <xuxin20010203@sjtu.edu.cn>
2023-04-17 19:14:38 +0800

    debug

--- codes/MainWidget.py
+++ codes/MainWidget.py
@@ -44,7 +44,7 @@ class MainWidget(QWidget):
         self.refinement_model_path = ""
         self.segment_model = U_Net(3, 3)
         self.segment_model.load_state_dict(torch.load(self.segment_model_path, map_location = self.device))
-        # self.segment_model.to(device=self.device)
+        self.segment_model.to(device=self.device)
         self.refinement_model = ""
         """
         初始化model,load参数,to device, eval
