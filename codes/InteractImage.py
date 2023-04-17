f3cffb562f4741af9ceae34dac1128ebeae77351
blueeeeeeeee <xuxin20010203@sjtu.edu.cn>
2023-04-17 20:19:26 +0800

    debug

--- codes/InteractImage.py
+++ codes/InteractImage.py
@@ -105,8 +105,8 @@ class InteractImage(object):
     
     def init_segment(self, model, device):
         # print("start segmentation")
-        TL_seeds = np.argwhere(self.TL_seeds[:,:,self.depth_current] == 1)
-        FL_seeds = np.argwhere(self.FL_seeds[:,:,self.depth_current] == 1)
+        # TL_seeds = np.argwhere(self.TL_seeds[:,:,self.depth_current] == 1)
+        # FL_seeds = np.argwhere(self.FL_seeds[:,:,self.depth_current] == 1)
         # """
         # 这里有大问题！！！
         # """
@@ -120,9 +120,9 @@ class InteractImage(object):
 
         cur_image = self.image[:,:,self.depth_current]
         last_image = self.image[:,:,self.depth_current]
-        last_label = region_grow(cur_image,TL_seeds) * self.TL_label + region_grow(cur_image, FL_seeds) * self.FL_label
+        # last_label = region_grow(cur_image,TL_seeds) * self.TL_label + region_grow(cur_image, FL_seeds) * self.FL_label
         # self.prediction[:,:,self.depth_current] = last_label
-        # last_label = self.seedsCoords2map()
+        last_label = self.seedsCoords2map()
         seeds_map = self.seedsCoords2map()
         # print("finish preparation")
 
@@ -135,13 +135,13 @@ class InteractImage(object):
             cur_image = self.image[:,:,i]
             flag = True
             prediction = last_label
-            if i != self.depth_current:
-                # TL_seeds = np.argwhere(self.TL_seeds[:,:,self.depth_current] == 1)
-                # FL_seeds = np.argwhere(self.FL_seeds[:,:,self.depth_current] == 1)
-                # prediction = region_grow(cur_image,TL_seeds) * self.TL_label + region_grow(cur_image, FL_seeds) * self.FL_label
-                # self.prediction[:,:,self.depth_current] = last_label
+            if i == self.depth_current:
+                indata = get_network_input_all(cur_image, np.argwhere(seeds_map > 0), seeds_map, window_transform_flag, feature_flag)
+                indata = torch.from_numpy(indata).unsqueeze(0).to(device=device,dtype=torch.float32)
+                prediction = get_prediction_all(model, indata)
+                prediction = np.uint8(prediction)
                 # print("get prediction - 1")
-            # else:
+            else:
                 flag, prediction,seeds_map = get_prediction_all_bidirectional(last_label, cur_image, last_image, window_transform_flag, feature_flag, sobel_flag, self.prediction, i - self.depth_current, device, model, seeds_case = 0)
             # print("get prediction - 2")
             if not flag:
