35fdbb18c69d74235a6840985218a7381f0d2b14
blueeeeeeeee <xuxin20010203@sjtu.edu.cn>
2023-04-17 14:25:09 +0800

    debug

--- codes/InteractImage.py
+++ codes/InteractImage.py
@@ -109,7 +109,7 @@ class InteractImage(object):
         # """
         # 这里有大问题！！！
         # """
-        print(TL_seeds.shape)
+        # print(TL_seeds.shape)
         # self.TL_seeds[:,:,self.depth_current] = seeds2map(TL_seeds, (self.height, self.width))
         # self.FL_seeds[:,:,self.depth_current] = seeds2map(FL_seeds, (self.height, self.width))
         # print("get init seeds")
@@ -118,37 +118,38 @@ class InteractImage(object):
         sobel_flag = True
 
         cur_image = self.image[:,:,self.depth_current]
-        last_label = region_grow(cur_image,TL_seeds) * self.TL_label + region_grow(cur_image, FL_seeds) * self.FL_label
         last_image = self.image[:,:,self.depth_current]
-        self.prediction[:,:,self.depth_current] = last_label
-        # last_label = self.seedsCoords2map()
-        # seeds_map = last_label
+        # last_label = region_grow(cur_image,TL_seeds) * self.TL_label + region_grow(cur_image, FL_seeds) * self.FL_label
+        # self.prediction[:,:,self.depth_current] = last_label
+        last_label = self.seedsCoords2map()
+        seeds_map = last_label
         print("finish preparation")
 
-        self.prediction2anotation(self.depth_current)
+        # self.prediction2anotation(self.depth_current)
         # print("finish anotation")
         # return 
 
-        for i in range(self.depth_current + 1, self.depth):
+        for i in range(self.depth_current, self.depth):
             print("start one piece")
             cur_image = self.image[:,:,i]
-            # flag = True
-            # prediction = last_label
-            # if i == self.depth_current:
-            #     indata = get_network_input_all(cur_image, np.argwhere(seeds_map > 0), seeds_map, window_transform_flag, feature_flag)
-            #     indata = torch.from_numpy(indata).unsqueeze(0).to(device=device,dtype=torch.float32)
-            #     prediction = get_prediction_all(model, indata)
-            #     prediction = np.uint8(prediction)
-            #     print("get prediction - 1")
-            # else:
-            flag, prediction,seeds_map = get_prediction_all_bidirectional(last_label, cur_image, last_image, window_transform_flag, feature_flag, sobel_flag, self.prediction, i - self.depth_current, device, model, seeds_case = 0)
-            print("get prediction - 2")
+            flag = True
+            prediction = last_label
+            if i == self.depth_current:
+                indata = get_network_input_all(cur_image, np.argwhere(seeds_map > 0), seeds_map, window_transform_flag, feature_flag)
+                indata = torch.from_numpy(indata).unsqueeze(0).to(device=device,dtype=torch.float32)
+                prediction = get_prediction_all(model, indata)
+                prediction = np.uint8(prediction)
+                print("get prediction - 1")
+            else:
+                flag, prediction,seeds_map = get_prediction_all_bidirectional(last_label, cur_image, last_image, window_transform_flag, feature_flag, sobel_flag, self.prediction, i - self.depth_current, device, model, seeds_case = 0)
+                print("get prediction - 2")
             if not flag:
                 break
             # print(np.unique(prediction, return_counts = True))
             # print(prediction.shape)
             self.prediction[:,:,i] = prediction
             self.prediction2anotation(i)
+            return 
             self.TL_seeds[:,:,i] = np.where(seeds_map == self.TL_label, self.TL_label, self.TL_seeds[:,:,i])
             self.FL_seeds[:,:,i] = np.where(seeds_map == self.FL_label, self.FL_label, self.FL_seeds[:,:,i])
             print("get seeds for each piece - 1")
