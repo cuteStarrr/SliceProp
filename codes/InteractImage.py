54d9dbac5b935b21e45849f874a1b684b0d29114
blueeeeeeeee <xuxin20010203@sjtu.edu.cn>
2023-04-17 13:51:36 +0800

    debug

--- codes/InteractImage.py
+++ codes/InteractImage.py
@@ -98,7 +98,7 @@ class InteractImage(object):
             tmp_FL = np.where(tmp_FL == [0, 0, 0], [0, 0, 0], list(self.FL_color))
             tmp = np.where(tmp_TL == list(self.TL_color), list(self.TL_color), tmp_FL)
             """for test"""
-            self.anotation[i,:,:,:] = tmp_TL
+            self.anotation[i,:,:,:] = tmp
             # self.anotation[i,:,:] = np.where(self.prediction[:,:,i] == self.TL_label, np.array(self.TL_color), self.anotation[i,:,:])
             # self.anotation[i,:,:] = np.where(self.prediction[:,:,i] == self.FL_label, np.array(self.FL_color), self.anotation[i,:,:])
     
