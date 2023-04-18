2e11f110912f7015d9d440014b30ffa8be833813
blueeeeeeeee <xuxin20010203@sjtu.edu.cn>
2023-04-18 21:15:51 +0800

    test gui pth12 focus on 2.h5 piece 151 and clean seeds region, watch on seeds

--- codes/InteractImage.py
+++ codes/InteractImage.py
@@ -150,7 +150,7 @@ class InteractImage(object):
         # self.FL_seeds[:,:,self.depth_current] = seeds2map(FL_seeds, (self.height, self.width))
         # print("get init seeds")
         window_transform_flag = True
-        clean_seeds_flag = True
+        clean_seeds_flag = False
         clean_region_flag = True
 
         cur_image = self.image[:,:,self.depth_current]
